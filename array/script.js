d2835499243a031508411c076bf7dc6fe47b3ff6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 12:59:45 -0300

    array

---
+++ array/script.js
@@ -0,0 +1,40 @@
+const nomes = ['Corinthians', 'Série B', 'Bambis', 'Bi Rebaixado']
+
+//atualizar dados 
+//nomes[1] = 'PÊNIS'
+
+//adicionar dados
+//nomes.push('SEXO A 3')
+
+//excluir valor,
+//nomes.pop()
+
+/*
+for(let i = 0; i < times.length; i++){
+    console.log(times[i])
+}
+*/
+
+/*
+//verifica todos os elementos do array
+times.forEach(function(meuTime){
+    console.log(meuTime)
+})
+
+times.forEach(elements=>{
+    console.log(elements)
+})
+*/
+
+/*
+//ele devolve um novo array
+const timesModificados= times.map(elements =>{
+    console.log(`Meu time é ${elements}`)
+})
+*/
+
+/*filtra o elemento do array
+console.log(TimeRanges.filter(elements=>{
+    return elements === 'Corinthians'
+}))
+*/
